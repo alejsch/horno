56d13d554f510f5064b715f0a3420ff237277808
alejsch <alejsch@gmail.com>
2018-11-24 13:09:39 -0300

    web

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'bcrypt', 'passlib', 'unidecode',
+        'bcrypt', 'passlib', 'unidecode', 'lxml',
     ],                 
 )
