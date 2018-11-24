4ec52f3568ada908a5cf6e00138ec1e2d97c2bec
alejsch <alejsch@gmail.com>
2018-11-24 13:33:02 -0300

    singleton

--- pyafs/datos/Encoding.py
+++ pyafs/datos/Encoding.py
@@ -12,104 +12,27 @@ class Encoding (Singleton):
     def __init__(self):
         
         self.TablaTran = {
-            '\x80':'C',
-            '\x82':'e',
-            '\xa0':'a',
-            '\xa1':'i', # char = ¡
-            '\xa2':'o', # char = ¢
-            '\xa3':'u', # char = £
-            '\xa4':'?', # char = ¤
-            '\xa5':'Ñ', # char = ¥
-            '\xa6':'?', # char = ¦
-            '\xa7':'?', # char = §
-            '\xa8':'?', # char = ¨
-            '\xa9':'?', # char = ©
-            '\xaa':'?', # char = ª
-            '\xab':'?', # char = «
-            '\xac':'?', # char = ¬
-            '\xad':'0', # char = 
-            '\xae':'?', # char = ®
-            '\xaf':'?', # char = ¯
-            '\xb0':'?', # char = °
-            '\xb1':'?', # char = ±
-            '\xb2':'?', # char = ²
-            '\xb3':'?', # char = ³
-            '\xb4':'?', # char = ´
-            '\xb5':'?', # char = µ
-            '\xb6':'?', # char = ¶
-            '\xb7':'?', # char = ·
-            '\xb8':'?', # char = ¸
-            '\xb9':'?', # char = ¹
-            '\xba':'?', # char = º
-            '\xbb':'?', # char = »
-            '\xbc':'?', # char = ¼
-            '\xbd':'?', # char = ½
-            '\xbe':'?', # char = ¾
-            '\xbf':'?', # char = ¿
-            '\xc0':'A', # char = À
-            '\xc1':'A', # char = Á
-            '\xc2':'A', # char = Â
-            '\xc3':'A', # char = Ã
-            '\xc4':'A', # char = Ä
-            '\xc5':'A', # char = Å
-            '\xc6':'AE', # char = Æ
-            '\xc7':'C', # char = Ç
-            '\xc8':'E', # char = È
-            '\xc9':'E', # char = É
-            '\xca':'E', # char = Ê
-            '\xcb':'E', # char = Ë
-            '\xcc':'I', # char = Ì
-            '\xcd':'I', # char = Í
-            '\xce':'I', # char = Î
-            '\xcf':'I', # char = Ï
-            '\xd0':'D', # char = Ð
-            '\xd1':'Ñ', # char = Ñ
-            '\xd2':'O', # char = Ò
-            '\xd3':'O', # char = Ó
-            '\xd4':'O', # char = Ô
-            '\xd5':'O', # char = Õ
-            '\xd6':'O', # char = Ö
-            '\xd7':'?', # char = ×
-            '\xd8':'O', # char = Ø
-            '\xd9':'U', # char = Ù
-            '\xda':'U', # char = Ú
-            '\xdb':'U', # char = Û
-            '\xdc':'U', # char = Ü
-            '\xdd':'Y', # char = Ý
-            '\xde':'?', # char = Þ
-            '\xdf':'ss', # char = ß
-            '\xe0':'a', # char = à
-            '\xe1':'a', # char = á
-            '\xe2':'a', # char = â
-            '\xe3':'a', # char = ã
-            '\xe4':'a', # char = ä
-            '\xe5':'a', # char = å
-            '\xe6':'ae', # char = æ
-            '\xe7':'c', # char = ç
-            '\xe8':'e', # char = è
-            '\xe9':'e', # char = é
-            '\xea':'e', # char = ê
-            '\xeb':'e', # char = ë
-            '\xec':'i', # char = ì
-            '\xed':'i', # char = í
-            '\xee':'i', # char = î
-            '\xef':'i', # char = ï
-            '\xf0':'d', # char = ð
-            '\xf1':'ñ', # char = ñ
-            '\xf2':'o', # char = ò
-            '\xf3':'o', # char = ó
-            '\xf4':'o', # char = ô
-            '\xf5':'o', # char = õ
-            '\xf6':'o', # char = ö
-            '\xf7':'?', # char = ÷
-            '\xf8':'o', # char = ø
-            '\xf9':'u', # char = ù
-            '\xfa':'u', # char = ú
-            '\xfb':'u', # char = û
-            '\xfc':'u', # char = ü
-            '\xfd':'y', # char = ý
-            '\xfe':'?', # char = þ
-            '\xff':'y', # char = ÿ
+            
+            # chars = 80 .. 8F
+            '\x80':'C', '\x82':'e',
+
+            # chars = A0 .. AF
+            '\xa0':'a', '\xa1':'i', '\xa2':'o', '\xa3':'u', '\xa4':'?', '\xa5':'Ñ', '\xa6':'?', '\xa7':'?', '\xa8':'?', '\xa9':'?', '\xaa':'?', '\xab':'?', '\xac':'?', '\xad':'0', '\xae':'?', '\xaf':'?',
+
+            # chars = B0 .. BF
+            '\xb0':'?', '\xb1':'?', '\xb2':'?', '\xb3':'?', '\xb4':'?', '\xb5':'?', '\xb6':'?', '\xb7':'?', '\xb8':'?', '\xb9':'?', '\xba':'?', '\xbb':'?', '\xbc':'?', '\xbd':'?', '\xbe':'?', '\xbf':'?',
+
+            # chars = C0 .. CF
+            '\xc0':'A', '\xc1':'A', '\xc2':'A', '\xc3':'A', '\xc4':'A', '\xc5':'A', '\xc6':'AE','\xc7':'C', '\xc8':'E', '\xc9':'E', '\xca':'E', '\xcb':'E', '\xcc':'I', '\xcd':'I', '\xce':'I', '\xcf':'I',
+
+            # chars = D0 .. DF
+            '\xd0':'D', '\xd1':'Ñ', '\xd2':'O', '\xd3':'O', '\xd4':'O', '\xd5':'O', '\xd6':'O', '\xd7':'?', '\xd8':'O', '\xd9':'U', '\xda':'U', '\xdb':'U', '\xdc':'U', '\xdd':'Y', '\xde':'?', '\xdf':'ss',
+
+            # chars = E0 .. EF
+            '\xe0':'a', '\xe1':'a', '\xe2':'a', '\xe3':'a', '\xe4':'a', '\xe5':'a', '\xe6':'ae','\xe7':'c', '\xe8':'e', '\xe9':'e', '\xea':'e', '\xeb':'e', '\xec':'i', '\xed':'i', '\xee':'i', '\xef':'i',
+            
+            # chars = F0 .. FF
+            '\xf0':'d', '\xf1':'ñ', '\xf2':'o', '\xf3':'o', '\xf4':'o', '\xf5':'o', '\xf6':'o', '\xf7':'?', '\xf8':'o', '\xf9':'u', '\xfa':'u', '\xfb':'u', '\xfc':'u', '\xfd':'y', '\xfe':'?', '\xff':'y',
         }
 
     #------------------------------------------------------------------------------------------
