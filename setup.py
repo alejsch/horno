56b1ea98d469860b99181829e0623e9dae04b16a
alejsch <alejsch@gmail.com>
2019-05-29 13:16:10 -0300

    +pkg

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'bcrypt', 'passlib', 'unidecode', 'libxml2', 'libxslt', 'lxml',
+        'bcrypt', 'passlib', 'unidecode', 'libxml2-dev', 'libxslt-dev', 'python-dev', 'lxml',
     ],                 
 )
