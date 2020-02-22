21c7dda551b5f7a1de9076ed90d046f50a077efb
alejsch <alejsch@gmail.com>
2020-02-22 20:44:28 -0300

    help url

--- horno/web/Http.py
+++ horno/web/Http.py
@@ -77,24 +77,49 @@ class HtmlHelper (metaclass=Singleton):
 class UrlHelper ():
     
     #------------------------------------------------------------------------------------------
-    def __init__(self, url):
+    def __init__(self, url, url_rel=''):
         
         from urllib.parse import urlparse
+        
         self.url_data = urlparse(url)
         self.esquema = self.url_data.scheme or 'http'
-        self.hostpuerto = self.url_data.netloc
-        self.host = self.url_data.hostname
+        self.hostpuerto = self.url_data.netloc or '.'
+        self.host = self.url_data.hostname or '.'
         self.ruta = self.url_data.path
         self.puerto = self.url_data.port
         self.params = self.url_data.params
         self.query = self.url_data.query
+        
+        if url_rel and self.host == '.':
+            url_data_rel = urlparse(url_rel)
+            self.esquema = url_data_rel.scheme or 'http'
+            self.hostpuerto = url_data_rel.netloc or '.'
+            self.host = url_data_rel.hostname or '.'
+            self.puerto = url_data_rel.port
+            if not self.ruta.startswith('/'):
+                self.ruta = '%s/%s' % (url_data_rel.path, self.url_data.path)
+        
+        self.ruta = self.ruta.replace('//', '/') 
 
     #------------------------------------------------------------------------------------------
-    def to_full(self):
+    def __str__(self):
+        
+        return self.full_path()
 
-        url_link_ok = '%s://%s%s/%s' % (
+    #------------------------------------------------------------------------------------------
+    def full_path(self):
+
+        url_link_ok = '%s://%s/%s%s' % (
             self.esquema, 
             self.hostpuerto,
-            self.ruta
+            self.ruta,
+            '?%s' % (self.query) if self.query else ''
         )
         return url_link_ok
+
+    #------------------------------------------------------------------------------------------
+    def prev_path(self):
+        
+        path = self.full_path()
+        path_partes = path.rsplit('/', 1)
+        return path_partes[0] if '.' in path_partes[-1] else path
